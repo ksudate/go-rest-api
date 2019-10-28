2732798ff731856b9e1243dd7b71c4b493f066d6
tmrekk121 <tmrekk121@outlook.jp>
2019-10-28 18:02:26 +0900

    Add Read function to DB

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
 import (
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
+
+// Splint expect
+type Splint struct {
+	ID        int       `gorm:"primary_key"`
+	SpNumber  int       `gorm:"type:int(11);"`
+	Content   string    `gorm:"type:varchar(255);"`
+	Kpt       string    `gorm:"type:varchar(255);"`
+	LineID    string    `gorm:"type:varchar(255);"`
+	CreatedAt time.Time `gorm:"type:datetime(6);"`
+	UpdatedAt time.Time `gorm:"type:datetime(6);"`
+}
 
 func main() {
 	db := ConnectDB()
@@ -12,6 +26,11 @@ func main() {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
+	})
+	r.GET("/splints", func(c *gin.Context) {
+		splints := []Splint{}
+		db.Find(&splints)
+		c.JSON(http.StatusOK, splints)
 	})
 	r.Run()
 }
